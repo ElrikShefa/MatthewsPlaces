7438a25eb10dc62b3078131da639dc9f948b7989
Matvey Chernyshov <Elrik.shefa@icloud.com>
2019-12-15 20:22:02 +0300

    initial

--- MatthewPlaces/ViewController.swift
+++ MatthewPlaces/ViewController.swift
@@ -8,13 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    let restaurantNames = [
+           "Burger Heroes", "Kitchen", "Bonsai", "Дастархан",
+           "Индокитай", "X.O", "Балкан Гриль", "Sherlock Holmes",
+           "Speak Easy", "Morris Pub", "Вкусные истории",
+           "Классик", "Love&Life", "Шок", "Бочка"
+       ]
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return restaurantNames.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        cell?.textLabel?.text = restaurantNames[indexPath.row]
+        cell?.imageView?.image = UIImage(named: restaurantNames[indexPath.row])
+        return cell!
+    }
 
 }
 
